d9165c70bd092a7a3f9535df4f02c2115474e827
Pavel 'Strajk' Dolecek <strajk@me.com>
2022-12-09 17:31:21 +0100

    fix(frontend,backend): Make item image required

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -8,7 +8,7 @@ var ItemSchema = new mongoose.Schema(
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: String,
+    image: { type: String, required: true, validate: /^https?:\/\//i }, // TODO: validate image url better
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
